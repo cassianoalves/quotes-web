80690d8d9e99c80ff0d77ab4e5c03ac726eba290
Cassiano Alves <cassiano.alves@gmail.com>
2015-09-20 12:11:14 -0300

    Tela de sign up

---
+++ app/scripts/controllers/signup.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name quotesWebApp.controller:SignupCtrl
+ * @description
+ * # SignupCtrl
+ * Controller of the quotesWebApp
+ */
+angular.module('quotesWebApp')
+  .controller('SignupCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
