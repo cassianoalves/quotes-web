98bec3f331640c16ca2645941191d0ed2c69e037
Cassiano Alves <cassiano.alves@gmail.com>
2015-10-22 01:22:19 -0200

    Evitando reload de mensagem infinita

--- app/scripts/controllers/random.js
+++ app/scripts/controllers/random.js
@@ -10,12 +10,20 @@
 angular.module('quotesWebApp')
   .controller('RandomCtrl', function ($scope, $routeParams, Book, $timeout) {
     var delay = 60;
+    var keepRepeat = true;
     if($routeParams.delay) {
       delay = Number($routeParams.delay);
     }
+
+    $scope.$on('$destroy', function () {
+      keepRepeat = false;
+    });
+
     function updateQuote() {
       $scope.randomQuote = Book.getRandomQuote({ book: $routeParams.id });
-      $timeout(updateQuote, delay * 1000);
+      if(keepRepeat) {
+        $timeout(updateQuote, delay * 1000);
+      }
     }
     updateQuote();
   });
