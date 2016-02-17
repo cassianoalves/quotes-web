e47ba8faf7aba38f1f25a1890db3cc75f355bb89
Cassiano Alves <cassiano.alves@gmail.com>
2016-02-17 01:40:29 -0200

    Melhorias no formulário, validação e envio da mensagem

--- app/scripts/controllers/mailme.js
+++ app/scripts/controllers/mailme.js
@@ -8,13 +8,33 @@
  * Controller of the quotesWebApp
  */
 angular.module('quotesWebApp')
-  .controller('MailmeCtrl', function ($scope, Contact) {
+  .controller('MailmeCtrl', function ($scope, Contact, DivToaster, ErrorMessage, $location) {
+    $scope.sending = false;
+
     $scope.contactMessage = {
       type: 'SUGESTION'
     };
 
+    $scope.validForm = function() {
+      return $scope.contactMessage.message && $scope.contactMessage.message.length > 5;
+    };
+
     $scope.send = function() {
-      Contact.mailme($scope.contactMessage);
-    }
+      $scope.sending = true;
+      Contact.mailme($scope.contactMessage,
+        function() {
+          $scope.sending = false;
+          DivToaster.toast('messageSubmit', 'success', 'Enviado',
+            function(){
+              $location.path('/').search('');
+            });
+        },
+        function(resp) {
+          DivToaster.toast('messageSubmit', 'error', ErrorMessage.getByCode(resp.data.code));
+          console.error('Error updating user', resp);
+          $scope.sending = false;
+        }
+      );
+    };
 
   });
