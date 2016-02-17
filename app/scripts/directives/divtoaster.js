e97ce96f2eee6cd10b28be7ab3fb5621d6288624
Cassiano Alves <cassiano.alves@gmail.com>
2016-02-17 01:39:44 -0200

    Callback após aparecer e desaparecer o toast

--- app/scripts/directives/divtoaster.js
+++ app/scripts/directives/divtoaster.js
@@ -7,7 +7,7 @@
  * # DivToaster
  */
 angular.module('quotesWebApp')
-  .directive('divToaster', function ($timeout) {
+  .directive('divToaster', function ($timeout, $rootScope) {
     return {
       template: '<div ng-show="show">' +
       '<div ng-show="type === \'success\'" class="text-success">{{ message }} <span class="glyphicon glyphicon-ok" aria-hidden="true"></span></div>' +
@@ -16,21 +16,22 @@ angular.module('quotesWebApp')
       restrict: 'E',
       scope: {}, // Para ter o próprio scope
       link: function postLink(scope, element, attrs) {
-        //element.text('this is the DivToaster directive');
+
         scope.show = false;
-        function blink() {
+        function blink(toast) {
           scope.show = true;
           $timeout(function () {
             scope.show = false;
+            $rootScope.$broadcast('DivToasterEnd', toast);
           }, 3000);
         }
 
         scope.$on('DivToasterCall', function(event, toast){
+          console.log('Recebeu evento', toast);
           if(attrs.id && toast.id && attrs.id === toast.id) {
-            //console.log('Recebeu evento', toast);
             scope.message = toast.message;
             scope.type = toast.type;
-            blink();
+            blink(toast);
           }
         });
       }
--- app/scripts/services/divtoaster.js
+++ app/scripts/services/divtoaster.js
@@ -9,7 +9,16 @@
  */
 angular.module('quotesWebApp')
   .service('DivToaster', function ($rootScope) {
-    this.toast = function (id, type, message) {
+    this.toast = function (id, type, message, callback) {
       $rootScope.$broadcast('DivToasterCall', {id: id, type: type, message: message});
+
+      var unregisterEvent = $rootScope.$on('DivToasterEnd', function(event, toast){
+        console.log('Recebeu evento DivToasterEnd', toast);
+        if(callback && (typeof callback === 'function') && id && toast.id && (id === toast.id)) {
+          callback(toast);
+        }
+        unregisterEvent();
+      });
+
     };
   });
